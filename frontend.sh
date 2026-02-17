12a7d3c2655adf56bd0db60395efd684deef1743
nikitha84 <nikitha84640@gmail.com>
2026-02-17 15:34:14 +0530

    shell-roboshop

--- frontend.sh
+++ frontend.sh
@@ -1,41 +1,52 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SCRIPT_DIR=$PWD
 
-LOGS_FOLDER="/var/log/shell-roboshop"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-MONGODB_HOST=mongodb.nikitha.fun
-SCRIPT_DIR=$PWD #/home/shell-roboshop/catalogue.sh
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
 fi
 
-VALIDATE(){ # functions receive inputs through args just like shell script args
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
-systemctl enable nginx 
+
+dnf module disable nginx -y &>>$LOGS_FILE
+dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+dnf install nginx -y &>>$LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx  &>>$LOGS_FILE
 systemctl start nginx 
+VALIDATE $? "Enabled and started nginx"
+
 rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+VALIDATE $? "Remove default content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$LOGS_FILE
+VALIDATE $? "Downloaded and unzipped frontend"
+
+rm -rf /etc/nginx/nginx.conf
+
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-systemctl restart nginx 
+VALIDATE $? "Copied our nginx conf file"
+
+systemctl restart nginx
+VALIDATE $? "Restarted Nginx"
